2a49d0e5c5257c02a63bd64c4c6f68fe016b0d06
gabeflomo <gabeflomo821@gmail.com>
2019-10-15 21:03:24 -0500

    updated project files

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,18 +1,23 @@
 ''' Main application for twitoff '''
 
 # imports 
-from flask import Flask
-#from .models import DB
+from flask import Flask, render_template, request
+from decouple import config
+from .models import DB, User
 
 #create app factory
 def create_app():
     ''' creates and configures an instance of a flask app '''
     app = Flask(__name__)
 
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV') #should change this later to production
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+    DB.init_app(app)
 
     @app.route("/")
     def root():
-        return "Welcome to our app"
+        user = User.query.all()
+        return render_template('base.html', title = "home",user = user)
     return app
     
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -1,15 +1,37 @@
-''' sql alchemy models for twitoff '''
 from flask_sqlalchemy import SQLAlchemy
+
+''' sql alchemy models for twitoff '''
+
 
 DB = SQLAlchemy()
 
 class User(DB.Model):
     ''' Twitter users that we pull and analyze '''
-    id = DB.Column(DB.Integer, primary_key = True)
-    name = DB.Column(DB.String(15), nullable = False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
     """Tweets"""
 
-    id = DB.Column(DB.Integer, primary_key = True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
+    user_id = DB.Column(DB.BigInteger,DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User',backref=DB.backref('tweet',lazy=True))
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
+
+
+def generate(name,tweet):
+
+    u1 = User(name=name)
+    t1 = Tweet(text=tweet)
+    DB.session.add(u1)
+    DB.session.add(t1)
+    DB.session.commit()
+    return "Generated a tweet by " + str(u1) + "with the tweet: " + str(tweet)
+
+#tweet_generator("Gabe","was good its ya boy")
